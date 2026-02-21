f8c4ea11144f3439f5b8c8adf5c6b2a9b41b126b
Jana <23-101109@students.eui.edu.eg>
2026-02-21 12:47:02 +0200

    adding copyrights component

---
+++ src/components/layout/copyrights.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './copyrights.css';
+
+const Copyrights = () => {
+    return ( 
+        <>
+        <article className='copyrights'>
+            <p>Copyright © Alliance Law 2026.</p>
+            <a href="#footer">Privacy Policy</a>
+        </article>
+        </>
+     );
+}
+ 
+export default Copyrights;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from '../components/layout/header';
 import './home.css';
 import Accessibility from '../components/layout/accessibility';
+import Copyrights from '../components/layout/copyrights';
 
 const Home = () => {
     return ( 
         <>
         <Accessibility />
         <Header />
+        <main>
+            <Copyrights />  
+        </main>
         </>
      );
 }
