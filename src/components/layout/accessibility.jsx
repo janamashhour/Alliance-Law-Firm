7296de0a28529abb75ae7204059acf986f1dd84b
Jana <23-101109@students.eui.edu.eg>
2026-02-21 12:36:22 +0200

    adding accessibility component

---
+++ src/components/layout/accessibility.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './accessibility.css';
+
+const Accessibility = () => {
+    return ( 
+        <>
+        <article>
+            <button>
+                <h6>Change Mode</h6>
+                <div className="sun"></div>
+            </button>
+        </article>
+        </>
+     );
+}
+ 
+export default Accessibility;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/layout/header';
 import './home.css';
+import Accessibility from '../components/layout/accessibility';
 
 const Home = () => {
     return ( 
         <>
+        <Accessibility />
         <Header />
         </>
      );
