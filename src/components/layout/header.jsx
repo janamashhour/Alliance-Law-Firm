1151341d9465f61803c5b0ff66732d7ac3131b5b
Jana <23-101109@students.eui.edu.eg>
2026-02-21 12:00:39 +0200

    dividing assets into folders and starting header component

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
     return ( 
         <>
+        <header>
+            <div className="logo-row">
+                <div className="logo"></div>
+                <div className="line"></div>
+                <h3>Alliance Law</h3>
+            </div>
+            <nav>
+                <ul>
+                </ul>
+            </nav>
+        </header>
         </>
      );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../components/layout/header';
+import './home.css';
 
 const Home = () => {
     return ( 
         <>
+        <Header />
         </>
      );
 }
