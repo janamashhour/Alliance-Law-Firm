6df9bb96c5fa3ce0205ada34996bf1967b4dd966
Jana <23-101109@students.eui.edu.eg>
2026-02-21 23:19:19 +0200

    making text styles and hero responsive

---
+++ src/components/layout/accessibility.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './accessibility.css';
+
+const Accessibility = () => {
+    return ( 
+        <>
+        <article>
+            <button className='mode-btn'>
+                <h6>Change Mode</h6>
+                <div className="sun"></div>
+            </button>
+        </article>
+        </>
+     );
+}
+ 
+export default Accessibility;
